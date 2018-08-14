c3aefc41c753791f8b7e36b4dda2de51c4486868
Chris Cooper <cjcooper252@gmail.com>
2018-08-14 15:53:17 -0700

    adds typography.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,11 @@ import Section from '../components/section'
 
 const IndexPage = () => (
   <div>
-    <Section color="blue" />
-    <Section color="red" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
   </div>
-
-  // <div>
-  //   <h1>Hi people</h1>
-  //   <p>Welcome to your new Gatsby site.</p>
-  //   <p>Now go build something great.</p>
-  //   <Link to="/page-2/">Go to page 2</Link>
-  // </div>
 )
 
 export default IndexPage
