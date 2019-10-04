5b45a37a724fac4c763a303924b2524e530b708c
Chris Cooper <cjcooper252@gmail.com>
2019-10-03 21:38:58 -0700

    base layout and header component

--- src/components/header.js
+++ src/components/header.js
@@ -2,41 +2,68 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = () => (
+  <header>
+    <div>
+      <div className="flex justify-between items-center mt-10">
+        <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl text-gray-900 font-medium md:font-normal leading-none">
+          <Link to="/">Chris Cooper</Link>
+        </h1>
+        <div className="lg:hidden">
+          <button type="button">
+            <svg
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              fill="none"
+            >
+              <path
+                d="M2 6H22V7H2V6ZM2 12H22V13H2V12ZM22 18H2V19H22V18Z"
+                class="fill-current text-gray-900"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
   </header>
 )
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
 export default Header
+
+// Header.propTypes = {
+//   siteTitle: PropTypes.string,
+// }
+
+// Header.defaultProps = {
+//   siteTitle: ``,
+// }
+{
+  /* <header
+style={{
+  background: `rebeccapurple`,
+  marginBottom: `1.45rem`,
+}}
+>
+<div
+  style={{
+    margin: `0 auto`,
+    maxWidth: 960,
+    padding: `1.45rem 1.0875rem`,
+  }}
+>
+  <h1 style={{ margin: 0 }}>
+    <Link
+      to="/"
+      style={{
+        color: `white`,
+        textDecoration: `none`,
+      }}
+    >
+      {siteTitle}
+    </Link>
+  </h1>
+</div> */
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,35 +7,51 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    {/* <SEO title="Home" /> */}
+    <div className="text-xl sm:text-2xl md:text-3xl lg:text-4xl md:font-light text-gray-700">
+      <p className="mt-10">Hi, I'm Chris Cooper.</p>
+      <p className="mt-6 sm:mt-8 md:mt-10">
+        I'm a web developer from the United Kingdom living in Vancouver, Canada.
+      </p>
+      <p className="mt-6 sm:mt-8 md:mt-10">
+        I'm passionate about JavaScript and writing clean, tested and
+        maintainable code.
+      </p>
     </div>
-
-    <div
-      className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-      role="alert"
-    >
-      <strong className="font-bold">Holy smokes!</strong>
-      <span className="block sm:inline">Something seriously bad happened.</span>
-      <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
-        <svg
-          className="fill-current h-6 w-6 text-red-500"
-          role="button"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-        >
-          <title>Close</title>
-          <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-        </svg>
-      </span>
-    </div>
-
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+{
+  /* <Layout>
+<SEO title="Home" />
+<h1>Hi people</h1>
+<p>Welcome to your new Gatsby site.</p>
+<p>Now go build something great.</p>
+<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+  <Image />
+</div>
+
+<div
+  className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+  role="alert"
+>
+  <strong className="font-bold">Holy smokes!</strong>
+  <span className="block sm:inline">Something seriously bad happened.</span>
+  <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+    <svg
+      className="fill-current h-6 w-6 text-red-500"
+      role="button"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+    >
+      <title>Close</title>
+      <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+    </svg>
+  </span>
+</div>
+
+<Link to="/page-2/">Go to page 2</Link>
+</Layout> */
+}
